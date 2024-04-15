efec15f361b24ed234737b50570e63f2f4e0436c
Bikal <82023507+Bikal-Adhikari@users.noreply.github.com>
2024-04-15 15:01:05 +1000

    Server initialised

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 import express from "express";
 const app = express();
+import morgan from "morgan";
+
 const PORT = process.env.PORT || 8000;
+
+//middlewares
+
+app.use(morgan("dev"));
 
 app.use("/", (req, res) => {
   res.json({
